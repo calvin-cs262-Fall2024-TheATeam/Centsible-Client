135a6b86d49ef3a9c89451255a0bd199c785bdb3
Ashley <ars73@calvin.edu>
2024-10-03 23:17:55 -0400

    Add tabs

--- Centsible/App.js
+++ Centsible/App.js
@@ -3,8 +3,88 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+
 
 const PlaceholderImage = require('./assets/background-img.png');
+
+const Tab = createBottomTabNavigator();
+
+function TransactionScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Transactions</Text>
+    </View>
+  );
+}
+
+function GoalsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Goals</Text>
+    </View>
+  );
+}
+function ReportScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Reports</Text>
+    </View>
+  );
+}
+
+function MyTabBar({ state, descriptors, navigation }) {
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+              ? options.title
+              : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{ flex: 1 }}
+          >
+            <Text style={{ color: isFocused ? '#673ab7' : '#222' }}>
+              {label}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -31,13 +111,15 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.footerContainer}>
-        <Button theme="primary" label="Sign up" />
-        <Button theme="primary" label="Login" />
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Transactions" component={TransactionScreen} />
+            <Tab.Screen name="Goals" component={GoalsScreen} />
+            <Tab.Screen name="Reports" component={ReportScreen} />
+          </Tab.Navigator>
+          <StatusBar style="auto" />
+    </NavigationContainer>
+    
   );
 }
 
