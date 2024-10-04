3be24ffcaa8f60fb68297d282227ad3b3e5b0b9f
Ashley <ars73@calvin.edu>
2024-10-04 17:25:53 -0400

    Refactor screens and styles into separate documents

--- Centsible/App.js
+++ Centsible/App.js
@@ -1,98 +1,20 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
+
+import { globalStyles } from './styles/globalStyles';
+import TransactionScreen from './screens/transaction';
+import GoalsScreen from './screens/goals';
+import ReportScreen from './screens/reports';
+import ProfileScreen from './screens/profile';
 
 
 const PlaceholderImage = require('./assets/background-img.png');
 
 const Tab = createBottomTabNavigator();
-
-function TransactionScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Transactions</Text>
-    </View>
-  );
-}
-
-function GoalsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Goals</Text>
-    </View>
-  );
-}
-function ReportScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Reports</Text>
-    </View>
-  );
-
-}function ProfileScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Profile</Text>
-    </View>
-  );
-}
-
-
-function MyTabBar({ state, descriptors, navigation }) {
-  return (
-    <View style={{ flexDirection: 'row' }}>
-      {state.routes.map((route, index) => {
-        const { options } = descriptors[route.key];
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-              ? options.title
-              : route.name;
-
-        const isFocused = state.index === index;
-
-        const onPress = () => {
-          const event = navigation.emit({
-            type: 'tabPress',
-            target: route.key,
-          });
-
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name);
-          }
-        };
-
-        const onLongPress = () => {
-          navigation.emit({
-            type: 'tabLongPress',
-            target: route.key,
-          });
-        };
-
-        return (
-          <TouchableOpacity
-            accessibilityRole="button"
-            accessibilityState={isFocused ? { selected: true } : {}}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            testID={options.tabBarTestID}
-            onPress={onPress}
-            onLongPress={onLongPress}
-            style={{ flex: 1 }}
-          >
-            <Text style={{ color: isFocused ? '#673ab7' : '#222' }}>
-              {label}
-            </Text>
-          </TouchableOpacity>
-        );
-      })}
-    </View>
-  );
-}
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -107,12 +29,12 @@ export default function App() {
 
   if (isLoading) {
     return (
-      <View style={styles.splashContainer}>
-        <View style={styles.imageContainer}>
+      <View style={globalStyles.splashContainer}>
+        <View style={globalStyles.imageContainer}>
           <ImageViewer placeholderImageSource={PlaceholderImage} />
         </View>
-        <View style={styles.textContainer}>
-          <Text style={styles.welcomeText}>Welcome to Centsible!</Text>
+        <View style={globalStyles.textContainer}>
+          <Text style={globalStyles.welcomeText}>Welcome to Centsible!</Text>
         </View>
       </View>
     );
@@ -131,36 +53,3 @@ export default function App() {
     
   );
 }
-
-const styles = StyleSheet.create({
-  splashContainer: {
-    flex: 1,
-    backgroundColor: 'purple',
-    alignItems: 'center',
-    justifyContent: 'flex-start', // Align items at the start
-  },
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  imageContainer: {
-    flex: 0.8, // Adjust this to control image height
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: 50, 
-  },
-  textContainer: {
-    alignItems: 'center', // Center horizontally
-    marginTop: 20, // Add some margin for spacing
-  },
-  welcomeText: {
-    color: 'white', // Change text color for better visibility
-    fontSize: 55,
-    textAlign: 'center', // Center text alignment
-  },
-  footerContainer: {
-    alignItems: 'center',
-  },
-});
